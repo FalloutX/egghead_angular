34b45aefba4736be39237db93c98d331e5fdbf51
Inderjit Sidhu <inderjit.sidhu@cube26.com>
2016-07-20 15:42:53 +0530

    006-Filters

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ angular.module('mainApp', [])
   .factory('Data', function() {
     return {message: "Coming from Service"};
   })
+  .filter('reverse', function() {
+    return function(text) {
+      return text.split("").reverse().join("");
+    };
+  })
   .controller('FirstCtrl', ['$scope', 'Data', function($scope, Data) {
     $scope.data = Data;
    }])
   .controller('SecondCtrl', ['$scope', 'Data', function($scope, Data) {
     $scope.data = Data;
-    $scope.reverseText = function(text) {
-      return text.split("").reverse().join("");
-    }
+
   }]);
