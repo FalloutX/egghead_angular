f49b0796ccfa3279e68fc49177607beb7465c330
Inderjit Sidhu <inderjit.sidhu@cube26.com>
2016-07-20 17:50:57 +0530

    009-Basic Angular Filters

--- app.js
+++ app.js
@@ -1,16 +1,72 @@
 angular.module('mainApp', [])
-  .factory('Data', function() {
-    return {message: "Coming from Service"};
-  })
-  .filter('reverse', function() {
-    return function(text) {
-      return text.split("").reverse().join("");
-    };
-  })
-  .controller('FirstCtrl', ['$scope', 'Data', function($scope, Data) {
-    $scope.data = Data;
-   }])
-  .controller('SecondCtrl', ['$scope', 'Data', function($scope, Data) {
-    $scope.data = Data;
+  .factory('Avengers', function() {
+    var Avengers = {};
+    Avengers.cast = [
+      {
+          name: "Robert Downey Jr.",
+          character: "Tony Stark / Iron Man"
+      },
+      {
+          name: "Chris Evans",
+          character: "Steve Rogers / Captain America}"
+      },
+      {
+          name: "Mark Ruffalo",
+          character: "Bruce Banner / The Hulk}"
+      },
+      {
+          name: "Chris Hemsworth",
+          character: "Thor"
+      },
+      {
+          name: "Scarlett Johansson",
+          character: "Natasha Romanoff / Black Widow"
+      },
+      {
+          name: "Jeremy Renner",
+          character: "Clint Barton / Hawkeye"
+      },
+      {
+          name: "Tom Hiddleston",
+          character: "Loki"
+      },
+      {
+          name: "Clark Gregg",
+          character: "Agent Phil Coulson"
+      },
+      {
+          name: "Cobie Smulders",
+          character: "Agent Maria Hill"
+      },
+      {
+          name: "Stellan Skarsgard",
+          character: "Selvig"
+      },
+      {
+          name: "Samuel L. Jackson",
+          character: "Nick Fury"
+      },
+      {
+          name: "Gwyneth Paltrow",
+          character: "Pepper Potts"
+      },
+      {
+          name: "Paul Bettany",
+          character: "Jarvis (voice)"
+      },
+      {
+          name: "Alexis Denisof",
+          character: "The Other"
+      },
+      {
+          name: "Tina Benko",
+          character: "NASA Scientist"
+      }
+    ];
 
+    return Avengers;
+  })
+
+  .controller('AvengersCtrl', ['$scope', 'Avengers', function($scope, Avengers) {
+    $scope.avengers = Avengers;
   }]);
