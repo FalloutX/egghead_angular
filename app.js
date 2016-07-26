50199bc82f47edcf5ed1cbe3c40f334685325cd7
FalloutX <inderjitsidhu@outlook.com>
2016-07-26 14:15:40 +0530

    016-Isolate Scope: & (Expression)

--- app.js
+++ app.js
@@ -1,72 +1,17 @@
-angular.module('mainApp', [])
-  .factory('Avengers', function() {
-    var Avengers = {};
-    Avengers.cast = [
-      {
-          name: "Robert Downey Jr.",
-          character: "Tony Stark / Iron Man"
+angular.module('choreApp', [])
+  .directive('kid', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        done: "&",
       },
-      {
-          name: "Chris Evans",
-          character: "Steve Rogers / Captain America}"
-      },
-      {
-          name: "Mark Ruffalo",
-          character: "Bruce Banner / The Hulk}"
-      },
-      {
-          name: "Chris Hemsworth",
-          character: "Thor"
-      },
-      {
-          name: "Scarlett Johansson",
-          character: "Natasha Romanoff / Black Widow"
-      },
-      {
-          name: "Jeremy Renner",
-          character: "Clint Barton / Hawkeye"
-      },
-      {
-          name: "Tom Hiddleston",
-          character: "Loki"
-      },
-      {
-          name: "Clark Gregg",
-          character: "Agent Phil Coulson"
-      },
-      {
-          name: "Cobie Smulders",
-          character: "Agent Maria Hill"
-      },
-      {
-          name: "Stellan Skarsgard",
-          character: "Selvig"
-      },
-      {
-          name: "Samuel L. Jackson",
-          character: "Nick Fury"
-      },
-      {
-          name: "Gwyneth Paltrow",
-          character: "Pepper Potts"
-      },
-      {
-          name: "Paul Bettany",
-          character: "Jarvis (voice)"
-      },
-      {
-          name: "Alexis Denisof",
-          character: "The Other"
-      },
-      {
-          name: "Tina Benko",
-          character: "NASA Scientist"
-      }
-    ];
-
-    return Avengers;
+      template: '<input type="text" ng-model="chore">' +
+                '{{ chore }}' +
+                '<div class="button" ng-click="done({chore:chore})">I\'m Done!</div>'
+    };
   })
-
-  .controller('AvengersCtrl', ['$scope', 'Avengers', function($scope, Avengers) {
-    $scope.avengers = Avengers;
+  .controller('choreCtrl', ['$scope', function ($scope) {
+    $scope.logChore = function(chore) {
+      alert(chore + " is done!");
+    };
   }]);
